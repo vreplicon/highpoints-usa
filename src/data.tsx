70cb1fefaa75ca05d8fd22c4493678bc81e126c0
Violet Replicon <vreplicon@gmail.com>
2020-03-14 17:28:37 -0700

    Updated names and coordinates of all points

--- src/data.tsx
+++ src/data.tsx
@@ -41,8 +41,8 @@ let AZ:Highpoint = {
 let AR:Highpoint = {
     name:'Magazine Mountain',
     state:'AR',
-    lat:35.34640,
-    long:111.6778,
+    lat:35.16720,
+    long:93.6447,
     elevation:2753,
     martian:2,
     gain:225
@@ -51,8 +51,8 @@ let AR:Highpoint = {
 let CA:Highpoint = {
     name:'Mount Whitney',
     state:'CA',
-    lat:35.34640,
-    long:111.6778,
+    lat:36.57860,
+    long:118.293,
     elevation:14494,
     martian:7,
     gain:6750
@@ -61,448 +61,448 @@ let CA:Highpoint = {
 let CO:Highpoint = {
     name:'Mount Elbert',
     state:'CO',
-    lat:35.34640,
-    long:111.6778,
+    lat:39.11780,
+    long:106.4447,
     elevation:14433,
     martian:6,
     gain:5000
 }
 
 let CT:Highpoint = {
-    name:'Humphreys Peak',
+    name:'Mount Frissell',
     state:'CT',
-    lat:35.34640,
-    long:111.6778,
+    lat:42.04940,
+    long:73.4846,
     elevation:2380,
     martian:3,
     gain:450
 }
 
 let DE:Highpoint = {
-    name:'Humphreys Peak',
+    name:'Ebright Azimuth',
     state:'DE',
-    lat:35.34640,
-    long:111.6778,
+    lat:39.83580,
+    long:75.522,
     elevation:448,
     martian:1,
     gain:10
 }
 
 let FL:Highpoint = {
-    name:'Humphreys Peak',
+    name:'Britton Hill',
     state:'FL',
-    lat:35.34640,
-    long:111.6778,
+    lat:30.98800,
+    long:86.2885,
     elevation:345,
     martian:1,
     gain:10
 }
 
 let GA:Highpoint = {
-    name:'Humphreys Peak',
+    name:'Brasstown Bald',
     state:'GA',
-    lat:35.34640,
-    long:111.6778,
+    lat:34.87410,
+    long:83.8108,
     elevation:4784,
     martian:2,
     gain:400
 }
 
 let HI:Highpoint = {
-    name:'Humphreys Peak',
+    name:'Mauna Kea',
     state:'HI',
-    lat:35.34640,
-    long:111.6778,
+    lat:19.82060,
+    long:155.46809,
     elevation:13769,
     martian:2,
     gain:230
 }
 
 let ID:Highpoint = {
-    name:'Humphreys Peak',
+    name:'Borah Peak',
     state:'ID',
-    lat:35.34640,
-    long:111.6778,
+    lat:44.13740,
+    long:113.781,
     elevation:12662,
     martian:8,
     gain:5500
 }
 
 let IL:Highpoint = {
-    name:'Humphreys Peak',
+    name:'Charles Mound',
     state:'IL',
-    lat:35.34640,
-    long:111.6778,
+    lat:42.50420,
+    long:90.2401,
     elevation:1235,
     martian:2,
     gain:275
 }
 
 let IN:Highpoint = {
-    name:'Humphreys Peak',
+    name:'Hoosier Hill',
     state:'IN',
-    lat:35.34640,
-    long:111.6778,
+    lat:40.00040,
+    long:84.8504,
     elevation:1257,
     martian:1,
     gain:10
 }
 
 let IA:Highpoint = {
-    name:'Humphreys Peak',
+    name:'Hawkeye Point',
     state:'IA',
-    lat:35.34640,
-    long:111.6778,
+    lat:43.46050,
+    long:95.7083,
     elevation:1670,
     martian:1,
     gain:10
 }
 
 let KS:Highpoint = {
-    name:'Humphreys Peak',
+    name:'Mount Sunflower',
     state:'KS',
-    lat:35.34640,
-    long:111.6778,
+    lat:39.02190,
+    long:102.037,
     elevation:4039,
     martian:1,
     gain:10
 }
 
 let KY:Highpoint = {
-    name:'Humphreys Peak',
+    name:'Black Mountain',
     state:'KY',
-    lat:35.34640,
-    long:111.6778,
+    lat:36.91420,
+    long:82.8939,
     elevation:4145,
     martian:1,
     gain:10
 }
 
 let LA:Highpoint = {
-    name:'Humphreys Peak',
+    name:'Driskill Mountain',
     state:'LA',
-    lat:35.34640,
-    long:111.6778,
+    lat:32.42440,
+    long:92.8965,
     elevation:535,
     martian:2,
     gain:150
 }
 
 let ME:Highpoint = {
-    name:'Humphreys Peak',
+    name:'Katahdin',
     state:'ME',
-    lat:35.34640,
-    long:111.6778,
+    lat:45.90440,
+    long:68.9228,
     elevation:5268,
     martian:5,
     gain:4200
 }
 
 let MD:Highpoint = {
-    name:'Humphreys Peak',
+    name:'Backbone Mountain',
     state:'MD',
-    lat:35.34640,
-    long:111.6778,
+    lat:39.14742,
+    long:79.57071,
     elevation:3360,
     martian:3,
     gain:750
 }
 
 let MA:Highpoint = {
-    name:'Humphreys Peak',
+    name:'Mount Greylock',
     state:'MA',
-    lat:35.34640,
-    long:111.6778,
+    lat:42.63750,
+    long:73.1667,
     elevation:3491,
     martian:1,
     gain:20
 }
 
 let MI:Highpoint = {
-    name:'Humphreys Peak',
+    name:'Mount Arvon',
     state:'MI',
-    lat:35.34640,
-    long:111.6778,
+    lat:46.75600,
+    long:88.156,
     elevation:1979,
     martian:1,
     gain:10
 }
 
 let MN:Highpoint = {
-    name:'Humphreys Peak',
+    name:'Eagle Mountain',
     state:'MN',
-    lat:35.34640,
-    long:111.6778,
+    lat:47.89745,
+    long:90.56057,
     elevation:2301,
     martian:4,
     gain:600
 }
 
 let MS:Highpoint = {
-    name:'Humphreys Peak',
+    name:'Woodall Mountain',
     state:'MS',
-    lat:35.34640,
-    long:111.6778,
+    lat:34.78790,
+    long:88.2418,
     elevation:806,
     martian:1,
     gain:10
 }
 
 let MO:Highpoint = {
-    name:'Humphreys Peak',
+    name:'Taum Sauk Mountain',
     state:'MO',
-    lat:35.34640,
-    long:111.6778,
+    lat:37.56660,
+    long:90.7333,
     elevation:1772,
     martian:1,
     gain:30
 }
 
 let MT:Highpoint = {
-    name:'Humphreys Peak',
+    name:'Granite Peak',
     state:'MT',
-    lat:35.34640,
-    long:111.6778,
+    lat:45.16306,
+    long:109.80771,
     elevation:12799,
     martian:9,
     gain:7700
 }
 
 let NE:Highpoint = {
-    name:'Humphreys Peak',
+    name:'Panorama Point',
     state:'NE',
-    lat:35.34640,
-    long:111.6778,
+    lat:41.00720,
+    long:104.0305,
     elevation:5424,
     martian:1,
     gain:10
 }
 
 let NV:Highpoint = {
-    name:'Humphreys Peak',
+    name:'Boundary Peak',
     state:'NV',
-    lat:35.34640,
-    long:111.6778,
+    lat:37.84610,
+    long:118.35,
     elevation:13140,
     martian:6,
     gain:4400
 }
 
 let NH:Highpoint = {
-    name:'Humphreys Peak',
+    name:'Mount Washington',
     state:'NH',
-    lat:35.34640,
-    long:111.6778,
+    lat:44.27059,
+    long:71.30327,
     elevation:6288,
     martian:1,
     gain:20
 }
 
 let NJ:Highpoint = {
-    name:'Humphreys Peak',
+    name:'High Point',
     state:'NJ',
-    lat:35.34640,
-    long:111.6778,
+    lat:41.31580,
+    long:74.68021,
     elevation:1803,
     martian:1,
     gain:40
 }
 
 let NM:Highpoint = {
-    name:'Humphreys Peak',
+    name:'Wheeler Peak',
     state:'NM',
-    lat:35.34640,
-    long:111.6778,
+    lat:36.55670,
+    long:105.4164,
     elevation:13161,
     martian:6,
     gain:3250
 }
 
 let NY:Highpoint = {
-    name:'Humphreys Peak',
+    name:'Mount Marcy',
     state:'NY',
-    lat:35.34640,
-    long:111.6778,
+    lat:44.11250,
+    long:73.9239,
     elevation:5343,
     martian:5,
     gain:3200
 }
 
 let NC:Highpoint = {
-    name:'Humphreys Peak',
+    name:'Mount Mitchell',
     state:'NC',
-    lat:35.34640,
-    long:111.6778,
+    lat:35.76484,
+    long:82.26512,
     elevation:6684,
     martian:1,
     gain:100
 }
 
 let ND:Highpoint = {
-    name:'Humphreys Peak',
+    name:'White Butte',
     state:'ND',
-    lat:35.34640,
-    long:111.6778,
+    lat:46.38703,
+    long:103.30257,
     elevation:3506,
     martian:2,
     gain:400
 }
 
 let OH:Highpoint = {
-    name:'Humphreys Peak',
+    name:'Campbell Hill',
     state:'OH',
-    lat:35.34640,
-    long:111.6778,
+    lat:40.36970,
+    long:83.7206,
     elevation:1550,
     martian:1,
     gain:10
 }
 
 let OK:Highpoint = {
-    name:'Humphreys Peak',
+    name:'Black Mesa',
     state:'OK',
-    lat:35.34640,
-    long:111.6778,
+    lat:36.93190,
+    long:102.9977,
     elevation:4973,
     martian:4,
     gain:775
 }
 
 let OR:Highpoint = {
-    name:'Humphreys Peak',
+    name:'Mount Hood',
     state:'OR',
-    lat:35.34640,
-    long:111.6778,
+    lat:45.37350,
+    long:121.6956,
     elevation:11239,
     martian:8,
     gain:5300
 }
 
 let PA:Highpoint = {
-    name:'Humphreys Peak',
+    name:'Mount Davis',
     state:'PA',
-    lat:35.34640,
-    long:111.6778,
+    lat:39.78610,
+    long:79.1761,
     elevation:3213,
     martian:1,
     gain:10
 }
 
 let RI:Highpoint = {
-    name:'Humphreys Peak',
+    name:'Jerimoth Hill',
     state:'RI',
-    lat:35.34640,
-    long:111.6778,
+    lat:41.84940,
+    long:71.7792,
     elevation:812,
     martian:1,
     gain:10
 }
 
 let SC:Highpoint = {
-    name:'Humphreys Peak',
+    name:'Sassafras Mountain',
     state:'SC',
-    lat:35.34640,
-    long:111.6778,
+    lat:35.06470,
+    long:82.7775,
     elevation:3560,
     martian:1,
     gain:10
 }
 
 let SD:Highpoint = {
-    name:'Humphreys Peak',
+    name:'Black Elk Peak',
     state:'SD',
-    lat:35.34640,
-    long:111.6778,
+    lat:43.86610,
+    long:103.5311,
     elevation:7242,
     martian:4,
     gain:1500
 }
 
 let TN:Highpoint = {
-    name:'Humphreys Peak',
+    name:'Clingmans Dome',
     state:'TN',
-    lat:35.34640,
-    long:111.6778,
+    lat:35.56269,
+    long:83.49863,
     elevation:6643,
     martian:2,
     gain:330
 }
 
 let TX:Highpoint = {
-    name:'Humphreys Peak',
+    name:'Guadalupe Peak',
     state:'TX',
-    lat:35.34640,
-    long:111.6778,
+    lat:31.89140,
+    long:104.86,
     elevation:8749,
     martian:5,
     gain:2950
 }
 
 let UT:Highpoint = {
-    name:'Humphreys Peak',
+    name:'Kings Peak',
     state:'UT',
-    lat:35.34640,
-    long:111.6778,
+    lat:40.77639,
+    long:110.37293,
     elevation:13528,
     martian:7,
     gain:5350
 }
 
 let VT:Highpoint = {
-    name:'Humphreys Peak',
+    name:'Mount Mansfield',
     state:'VT',
-    lat:35.34640,
-    long:111.6778,
+    lat:44.54400,
+    long:72.815,
     elevation:4393,
     martian:3,
     gain:550
 }
 
 let VA:Highpoint = {
-    name:'Humphreys Peak',
+    name:'Mount Rogers',
     state:'VA',
-    lat:35.34640,
-    long:111.6778,
+    lat:36.66005,
+    long:81.5449,
     elevation:5729,
     martian:4,
     gain:1500
 }
 
 let WA:Highpoint = {
-    name:'Humphreys Peak',
+    name:'Mount Rainier',
     state:'WA',
-    lat:35.34640,
-    long:111.6778,
+    lat:46.85280,
+    long:121.759,
     elevation:14411,
     martian:9,
     gain:9100
 }
 
 let WV:Highpoint = {
-    name:'Humphreys Peak',
+    name:'Spruce Knob',
     state:'WV',
-    lat:35.34640,
-    long:111.6778,
+    lat:38.69976,
+    long:79.53299,
     elevation:4863,
     martian:1,
     gain:20
 }
 
 let WI:Highpoint = {
-    name:'Humphreys Peak',
+    name:'Timms Hill',
     state:'WI',
-    lat:35.34640,
-    long:111.6778,
+    lat:45.45100,
+    long:90.1954,
     elevation:1951,
     martian:1,
     gain:120
 }
 
 let WY:Highpoint = {
-    name:'Humphreys Peak',
+    name:'Gannett Peak',
     state:'WY',
-    lat:35.34640,
-    long:111.6778,
+    lat:43.18440,
+    long:109.65429,
     elevation:13804,
     martian:9,
     gain:8650
